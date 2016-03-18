c1357e10aff16b8aaecf265da54c1c0351242b5c
monkeyGoCrazy <mengleisun1991@gmail.com>
2016-03-18 12:10:46 -0400

    Add user controllers

---
+++ src/main/java/com/dbms/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.dbms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+/**
+ * Created by mengleisun on 3/16/16.
+ */
+@Controller
+public class UserController {
+//    @RequestMapping("/user")
+//    public Principal user(Principal user) {
+//        return user;
+//    }
+}
